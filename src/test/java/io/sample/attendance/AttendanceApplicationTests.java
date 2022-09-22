d40646dfae4620777f65db89d63280af6119e42b
choi-ys <project.log.062@gmail.com>
2022-09-22 13:58:50 +0900

    Initial commit

---
+++ src/test/java/io/sample/attendance/AttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sample.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
