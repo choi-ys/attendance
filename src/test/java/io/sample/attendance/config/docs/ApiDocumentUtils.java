22ef4c542c8c0043f90ac6f48e1f0de7e5684214
choi-ys <project.log.062@gmail.com>
2022-10-17 23:55:39 +0900

    feat(#10): Spring REST Docs 관련 설정 및 코드 제거

--- src/test/java/io/sample/attendance/config/docs/ApiDocumentUtils.java
+++ src/test/java/io/sample/attendance/config/docs/ApiDocumentUtils.java
@@ -1,59 +1,21 @@
 package io.sample.attendance.config.docs;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
 import com.epages.restdocs.apispec.ResourceSnippet;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
-import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
 import org.springframework.restdocs.snippet.Attributes.Attribute;
 import org.springframework.restdocs.snippet.Snippet;
 
 public interface ApiDocumentUtils {
-    String BASE_HOST = "attendance.io";
     String DOCUMENT_IDENTIFIER = "{class-name}/{method-name}";
 
-    static OperationRequestPreprocessor getDocumentRequest() {
-        return preprocessRequest(
-            modifyUris()
-                .scheme(scheme())
-                .host(host())
-                .removePort(),
-            prettyPrint());
-    }
-
-    static OperationResponsePreprocessor getDocumentResponse() {
-        return preprocessResponse(
-            modifyUris()
-                .scheme(scheme())
-                .host(host())
-                .removePort(),
-            prettyPrint());
-    }
-
     static RestDocumentationResultHandler createDocumentBySnippets(Snippet... snippets) {
-        return document(
-            DOCUMENT_IDENTIFIER,
-            getDocumentRequest(),
-            getDocumentResponse(),
-            snippets
-        );
+        return document(DOCUMENT_IDENTIFIER, snippets);
     }
 
     static RestDocumentationResultHandler createDocumentByResourceSnippet(ResourceSnippet resourceSnippet) {
         return document(DOCUMENT_IDENTIFIER, resourceSnippet);
-    }
-
-    private static String scheme() {
-        return "http";
-    }
-
-    private static String host() {
-        return String.format("dev-api.%s", BASE_HOST);
     }
 
     static Attribute format(String value) {
--- src/test/java/io/sample/attendance/generator/docs/AttendanceDocsGenerator.java
+++ src/test/java/io/sample/attendance/generator/docs/AttendanceDocsGenerator.java
@@ -1,19 +1,23 @@
 package io.sample.attendance.generator.docs;
 
-import static io.sample.attendance.config.docs.ApiDocumentUtils.createDocument;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static io.sample.attendance.config.docs.ApiDocumentUtils.createDocumentByResourceSnippet;
+import static io.sample.attendance.config.docs.ApiDocumentUtils.createDocumentBySnippets;
 import static io.sample.attendance.config.docs.ApiDocumentUtils.format;
 import static io.sample.attendance.generator.docs.AttendanceFieldDescriptor.attendanceResponseFieldWithPath;
 import static io.sample.attendance.generator.docs.common.CommonFieldDescriptor.commonErrorFieldWithPath;
 import static io.sample.attendance.generator.docs.common.CommonFieldDescriptor.commonPaginationFieldWithPath;
 import static io.sample.attendance.generator.docs.common.CommonFieldDescriptor.invalidErrorFieldWithPath;
+import static io.sample.attendance.generator.docs.common.CommonHeaderSnippetGenerator.defaultRequestHeaderDescriptors;
 import static io.sample.attendance.generator.docs.common.CommonHeaderSnippetGenerator.defaultRequestHeaderSnippet;
+import static io.sample.attendance.generator.docs.common.CommonHeaderSnippetGenerator.defaultResponseHeaderDescriptor;
 import static io.sample.attendance.generator.docs.common.CommonHeaderSnippetGenerator.defaultResponseHeaderSnippet;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import java.util.Arrays;
@@ -25,21 +29,27 @@ import org.springframework.restdocs.payload.FieldDescriptor;
 
 public class AttendanceDocsGenerator {
     public static RestDocumentationResultHandler saveAttendanceDocument() {
-        return createDocument(
-            defaultRequestHeaderSnippet(),
-            defaultResponseHeaderSnippet(),
-            requestFields(
-                fieldWithPath("startAt").description("근무 시작 시간").attributes(format("YYYY-MM-DD hh:mm:ss")),
-                fieldWithPath("endAt").description("근무 종료 시간").attributes(format("YYYY-MM-DD hh:mm:ss"))
-            ),
-            responseFields(
-                attendanceResponseFieldWithPath()
+        return createDocumentByResourceSnippet(
+            resource(
+                ResourceSnippetParameters.builder()
+                    .summary("출결 등록 API")
+                    .description("POST 요청을 사용하여 출결을 등록할 수 있습니다.")
+                    .requestHeaders(defaultRequestHeaderDescriptors())
+                    .responseHeaders(defaultResponseHeaderDescriptor())
+                    .requestFields(
+                        fieldWithPath("startAt").description("근무 시작 시간").attributes(format("YYYY-MM-DD hh:mm:ss")),
+                        fieldWithPath("endAt").description("근무 종료 시간").attributes(format("YYYY-MM-DD hh:mm:ss"))
+                    )
+                    .responseFields(
+                        attendanceResponseFieldWithPath()
+                    )
+                    .build()
             )
         );
     }
 
     public static RestDocumentationResultHandler emptyRegisterAttendanceRequestDocument() {
-        return createDocument(
+        return createDocumentBySnippets(
             defaultRequestHeaderSnippet(),
             defaultResponseHeaderSnippet(),
             responseFields(
@@ -49,14 +59,14 @@ public class AttendanceDocsGenerator {
     }
 
     public static RestDocumentationResultHandler invalidRegisterAttendanceRequestDocument() {
-        return createDocument(
+        return createDocumentBySnippets(
             defaultRequestHeaderSnippet(),
             defaultResponseHeaderSnippet(),
             requestFields(
                 fieldWithPath("startAt").description("근무 시작 시간").attributes(format("YYYY-MM-DD hh:mm:ss")),
                 fieldWithPath("endAt").description("근무 종료 시간").attributes(format("YYYY-MM-DD hh:mm:ss"))
             ),
-            responseFields(
+            relaxedResponseFields(
                 Stream.concat(
                     commonErrorFieldWithPath().stream(),
                     invalidErrorFieldWithPath().stream()
@@ -66,46 +76,71 @@ public class AttendanceDocsGenerator {
     }
 
     public static RestDocumentationResultHandler getAnAttendanceDocument() {
-        return createDocument(
+        return createDocumentByResourceSnippet(
+            resource(
+                ResourceSnippetParameters.builder()
+                    .summary("출결 조회 API")
+                    .description("GET 요청을 사용하여 출결을 조회 할 수 있습니다.")
+                    .requestHeaders(defaultRequestHeaderDescriptors())
+                    .responseHeaders(defaultResponseHeaderDescriptor())
+                    .pathParameters(
+                        parameterWithName("id").description("출결 번호")
+                    )
+                    .responseFields(
+                        attendanceResponseFieldWithPath()
+                    )
+                    .build()
+            )
+        );
+    }
+
+    public static RestDocumentationResultHandler notFoundAttendanceRequestDocument() {
+        return createDocumentBySnippets(
             defaultRequestHeaderSnippet(),
             defaultResponseHeaderSnippet(),
             pathParameters(
                 parameterWithName("id").description("출결 번호")
             ),
             responseFields(
-                attendanceResponseFieldWithPath()
+                commonErrorFieldWithPath()
             )
         );
     }
 
     public static RestDocumentationResultHandler getAttendancesDocument() {
-        return createDocument(
-            defaultRequestHeaderSnippet(),
-            requestParameters(
-                parameterWithName("yearMonth").description("조회 년월").attributes(format("YYYY-MM"))
-            ),
-            defaultResponseHeaderSnippet(),
-            responseFields(
-                Stream.concat(
-                    commonPaginationFieldWithPath().stream(),
-                    Stream.of(
-                        fieldWithPath("elements[*].id").description("출결 번호"),
-                        fieldWithPath("elements[*].startAt").description("근무 시작 시간"),
-                        fieldWithPath("elements[*].endAt").description("근무 종료 시간"),
-                        fieldWithPath("elements[*].workDuration.hour").description("근무 소요 시간"),
-                        fieldWithPath("elements[*].workDuration.minute").description("근무 소요 분"),
-                        fieldWithPath("elements[*].basicPay").description("기본 수당"),
-                        fieldWithPath("elements[*].totalPay").description("전체 급여"),
-                        fieldWithPath("elements[*].extraWorks[*]").description("추가 근무 목록"),
-                        fieldWithPath("elements[*].extraWorks[*].id").description("추가 근무 번호"),
-                        fieldWithPath("elements[*].extraWorks[*].startAt").description("추가 근무 시작 시간"),
-                        fieldWithPath("elements[*].extraWorks[*].endAt").description("추가 근무 종료 시간"),
-                        fieldWithPath("elements[*].extraWorks[*].workDuration.hour").description("추가 근무 소요 시간"),
-                        fieldWithPath("elements[*].extraWorks[*].workDuration.minute").description("추가 근무 소요 분"),
-                        fieldWithPath("elements[*].extraWorks[*].extraWorkType").description("추가 근무 타입"),
-                        fieldWithPath("elements[*].extraWorks[*].extraPay").description("추가 수당")
+        return createDocumentByResourceSnippet(
+            resource(
+                ResourceSnippetParameters.builder()
+                    .summary("기준년월의 출결 목록 조회 API")
+                    .description("GET 요청을 사용하여 기준년월의 출결 정보를 조회할 수 있습니다.")
+                    .requestHeaders(defaultRequestHeaderDescriptors())
+                    .responseHeaders(defaultResponseHeaderDescriptor())
+                    .requestParameters(
+                        parameterWithName("yearMonth").description("조회 년월").attributes(format("YYYY-MM"))
                     )
-                ).collect(Collectors.toList())
+                    .responseFields(
+                        Stream.concat(
+                            commonPaginationFieldWithPath().stream(),
+                            Stream.of(
+                                fieldWithPath("elements[*].id").description("출결 번호"),
+                                fieldWithPath("elements[*].startAt").description("근무 시작 시간"),
+                                fieldWithPath("elements[*].endAt").description("근무 종료 시간"),
+                                fieldWithPath("elements[*].workDuration.hour").description("근무 소요 시간"),
+                                fieldWithPath("elements[*].workDuration.minute").description("근무 소요 분"),
+                                fieldWithPath("elements[*].basicPay").description("기본 수당"),
+                                fieldWithPath("elements[*].totalPay").description("전체 급여"),
+                                fieldWithPath("elements[*].extraWorks[*]").description("추가 근무 목록"),
+                                fieldWithPath("elements[*].extraWorks[*].id").description("추가 근무 번호"),
+                                fieldWithPath("elements[*].extraWorks[*].startAt").description("추가 근무 시작 시간"),
+                                fieldWithPath("elements[*].extraWorks[*].endAt").description("추가 근무 종료 시간"),
+                                fieldWithPath("elements[*].extraWorks[*].workDuration.hour").description("추가 근무 소요 시간"),
+                                fieldWithPath("elements[*].extraWorks[*].workDuration.minute").description("추가 근무 소요 분"),
+                                fieldWithPath("elements[*].extraWorks[*].extraWorkType").description("추가 근무 타입"),
+                                fieldWithPath("elements[*].extraWorks[*].extraPay").description("추가 수당")
+                            )
+                        ).collect(Collectors.toList())
+                    )
+                    .build()
             )
         );
     }
